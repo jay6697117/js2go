648b7604399c1fe5e26a4cd2b73fda0157d54870
SteveZhang <871506263@qq.com>
2025-11-10 11:06:21 +0800

    1

--- http-server.js
+++ http-server.js
@@ -1,18 +1,3 @@
-package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
-	fmt.Println("Go HTTP Server running on http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
-}
 const http = require('http');
 
 const server = http.createServer((req, res) => {
